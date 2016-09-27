fddbed2d98332f62e78eb4accf8a4c1061dace33
Robert Ellis <robmadeyou@gmail.com>
2016-09-27 12:26:29 +0100

    In other hand, being smart might be harder

--- pool.php
+++ pool.php
@@ -1,14 +1,8 @@
 <?php
 
-if (isset($_GET['id']) && isValidMd5($_GET['id'])) {
+if (isset($_GET['id'])) {
     $id = md5($_GET['id']);
     $data = file_get_contents('./logs/' . $id . '.txt');
     print nl2br($data);
 }
-
-function isValidMd5($md5 = '')
-{
-    return strlen($md5) == 32 && ctype_xdigit($md5);
-}
-
 ?>
